1c86348facfa6324720cc1092e719c61fd4f0646
lava <lavakumar.khanapur@gmail.com>
2022-12-08 18:13:54 +0530

    this is test

---
+++ src/test/java/com/globallogic/realEstateProject/RealEstateProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globallogic.realEstateProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
